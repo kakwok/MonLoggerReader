e7648a54154138346453e16481c864c6f9bf74e1
kakwok <martin.kwok@cern.ch>
2018-10-16 13:48:33 +0200

    first commit for all RBXes

--- monlogReader.py
+++ monlogReader.py
@@ -15,6 +15,17 @@ from ROOT import PyConfig
 PyConfig.IgnoreCommandLineOptions = True
 from ROOT import *
 
+#Return a list of all RBX names
+def getAllRBXes():
+    rbxes =[]
+    for end in "MP":
+        for sector in range(1, 19)+[29,30]:
+            rbx = "HE%1s%02d" % (end, sector)
+            if rbx in ["HEM15","HEM16","HEM30","HEP29"]: continue   #skip HEM15,16
+            rbxes.append(rbx)
+    return rbxes
+
+
 def buildname(elements):
     listTojoin = []
     for el in elements:
@@ -154,6 +165,7 @@ def parsed_args():
     parser.add_argument("--startTime"   , help="StartTime to look for lines, format: yyyy-mm-dd HH:MM")
     parser.add_argument("--endTime"     , help="EndTime   to look for lines, format: yyyy-mm-dd HH:MM")
     parser.add_argument("--RBX"         , help="select an RBX "  ,default="")
+    parser.add_argument("--RBXes"       , help="Loop all RBXes", action="store_true", default=False)
     parser.add_argument("--crate"       , help="select an crate ",default="")
     parser.add_argument("--ymin"        , help="min y-value on plots", type=float, default=1E-6)
     parser.add_argument("--ymax"        , help="max y-value on plots", type=float, default=1E5)
@@ -164,49 +176,8 @@ def parsed_args():
     parser.add_argument("--diff"        , help="plot the diff of columns", action="store_true", default=False)
     return parser.parse_args()
 
-def main(args):
-    print datetime.now()
-    outname = os.path.join(args.odir,buildname([args.crate, args.RBX,"multiGraph",args.printConfig.split("/")[-1].replace(".json",""),args.startTime.split(" ")[0]]))
-    
-    columnToShow = []
-    flist        = []
-    if(args.printConfig is not None):
-        printConfigFile=open(args.printConfig)
-        printConfig       = json.load(printConfigFile)
-        columnToShow      = printConfig["columnToShow"]
-        if "columnValueFilter" in printConfig.keys(): 
-            columnValueFilter = printConfig["columnValueFilter"]
-        if "flist" in printConfig.keys(): 
-            flist = printConfig["flist"]
-            print flist
-    
-    columnValueFilters=[]
-    
-    if args.endTime is not None:
-        endTime      = datetime.strptime(args.endTime,"%Y-%m-%d %H:%M")
-        endTimefilter   = {"colname":"timestamp","value":endTime,"operator":"<="}
-        columnValueFilters.append(endTimefilter)
-    if args.startTime is not None:
-        startTime      = datetime.strptime(args.startTime,"%Y-%m-%d %H:%M")
-        startTimefilter   = {"colname":"timestamp","value":startTime,"operator":">="}
-        columnValueFilters.append(startTimefilter)
-    if args.RBX is not "":
-        RBXfilter = {"colname":"RBX","value":args.RBX,"operator":"=="}
-        columnValueFilters.append(RBXfilter)
-    if args.crate is not "":
-        cratefilter = {"colname":"CRATE","value":args.crate,"operator":"=="}
-        columnValueFilters.append(cratefilter)
-    
-    df_list = []
-    for f in flist:
-        if f[0]=="#": continue
-        df_list.append( getDataFrame(f,columnToShow,columnValueFilters))
-    
-    
-    frames = pd.concat( df_list )
-    frames.sort_values( by='dates')
-    
-    
+def plotdataframe(frames,args,columnToShow,columnValueFilters,outname):
+    outf    = TFile(outname+".root","RECREATE")
     dfcut = buildselection(frames,columnValueFilters) 
     if dfcut.empty:
         print "No entries in the table fits the requirements:"
@@ -235,9 +206,68 @@ def main(args):
                     dfcut[col+"_diff"] = dfcut[col].diff()              # make a new diff column
                 else:
                     print "columne: %s  is not int/float, not performing diff."%col
+        makeplots(dfcut, columnToShow, columnValueFilters, outname, args.ymin, args.ymax, args.png, args.diff)
+    outf.Close()
+
+def main(args):
+    print datetime.now()
+    outname = os.path.join(args.odir,buildname([args.crate, args.RBX,"multiGraph",args.printConfig.split("/")[-1].replace(".json",""),args.startTime.split(" ")[0]]))
     
-    outf    = TFile(outname+".root","RECREATE")
-    makeplots(dfcut, columnToShow, columnValueFilters, outname, args.ymin, args.ymax, args.png, args.diff)
+    columnToShow = []
+    flist        = []
+    if(args.printConfig is not None):
+        printConfigFile=open(args.printConfig)
+        printConfig       = json.load(printConfigFile)
+        columnToShow      = printConfig["columnToShow"]
+        if "columnValueFilter" in printConfig.keys(): 
+            columnValueFilter = printConfig["columnValueFilter"]
+        if "flist" in printConfig.keys(): 
+            flist = printConfig["flist"]
+            print flist
+    
+    columnValueFilters=[]
+    timeFilters =[]
+    
+    if args.endTime is not None:
+        endTime      = datetime.strptime(args.endTime,"%Y-%m-%d %H:%M")
+        endTimefilter   = {"colname":"timestamp","value":endTime,"operator":"<="}
+        columnValueFilters.append(endTimefilter)
+        timeFilters.append(endTimefilter)
+    if args.startTime is not None:
+        startTime      = datetime.strptime(args.startTime,"%Y-%m-%d %H:%M")
+        startTimefilter   = {"colname":"timestamp","value":startTime,"operator":">="}
+        columnValueFilters.append(startTimefilter)
+        timeFilters.append(startTimefilter)
+    if args.RBX is not "":
+        RBXfilter = {"colname":"RBX","value":args.RBX,"operator":"=="}
+        columnValueFilters.append(RBXfilter)
+    if args.crate is not "":
+        cratefilter = {"colname":"CRATE","value":args.crate,"operator":"=="}
+        columnValueFilters.append(cratefilter)
+    
+    df_list = []
+    for f in flist:
+        if f[0]=="#": continue
+        df_list.append( getDataFrame(f,columnToShow,columnValueFilters))
+    
+    
+    frames = pd.concat( df_list )
+    frames.sort_values( by='dates')
+    
+    if not args.RBXes:
+        plotdataframe(frames,args,columnToShow,columnValueFilters,outname)
+    else:
+        RBXes     = getAllRBXes()
+        loopfilter = timeFilters
+        for RBX in RBXes: 
+            print "Making plots for RBX= ",RBX
+            RBXfilter = {"colname":"RBX","value":RBX,"operator":"=="}
+            loopfilter.append(RBXfilter)
+            print loopfilter
+            outname = os.path.join(args.odir,buildname([args.crate, RBX,"multiGraph",args.printConfig.split("/")[-1].replace(".json",""),args.startTime.split(" ")[0]]))
+            plotdataframe(frames,args,columnToShow,loopfilter,outname)
+            loopfilter.pop()
+        
     # TODO: implement matplotlib function to make nice-looking plots
     #makeplots_matplotlib(dfcut, columnToShow, columnValueFilters, "test", args.ymin, args.ymax)
     print datetime.now()
